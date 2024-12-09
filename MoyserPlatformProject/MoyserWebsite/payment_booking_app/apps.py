60a8e82357df6cea976acfc7cc1ad2ac5c7aa07a
MarwahDesigner90 <marwaty666@gmail.com>
2024-12-09 22:13:56 +0300

    Start configuring platform applications

---
+++ MoyserPlatformProject/MoyserWebsite/payment_booking_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PaymentBookingAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'payment_booking_app'
---
+++ MoyserPlatformProject/MoyserWebsite/service_request_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ServiceRequestAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'service_request_app'
