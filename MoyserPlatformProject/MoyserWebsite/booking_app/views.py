07681265550e87975185a4c356aa94513ca2dc28
Alhanouf <hnno3040@gmail.com>
2024-12-11 17:59:12 +0300

    create booking model, urls and views

--- MoyserPlatformProject/MoyserWebsite/booking_app/views.py
+++ MoyserPlatformProject/MoyserWebsite/booking_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from .models import Booking
 # Create your views here.
+
+@login_required
+def book_companion_view(request):
+    return render(request, 'booking_app/create_booking.html')
+
+
+def booking_history_user_view(request):
+    return render(request, 'booking_app/user_booking_history.html')
+
+@login_required
+def booking_history_companion_view(request):
+    return render(request, 'booking_app/companion_booking_history.html')
+
+@login_required
+def companion_list_view(request):
+    return render(request, 'booking_app/companion_list.html')
