5d15532d2edc65b333b4a7214f2305d4ec33fd50
MarwahDesigner90 <marwaty666@gmail.com>
2024-12-18 14:10:51 +0300

    some changes in main page in design

--- MoyserPlatformProject/MoyserWebsite/main_app/views.py
+++ MoyserPlatformProject/MoyserWebsite/main_app/views.py
@@ -5,37 +5,15 @@ from account_app.models import Companion, Skill ,  DisabilityUser
 # Create your views here.
 
 def home_view(request:HttpRequest):
-# adding filter code here  
-    # testimonials = Testimonial.objects.all().order_by('-created_at')[:10]  # Limit to 10 recent testimonials
-    
-    return render(request,"main_app/home.html",{'cities':Companion.CITY_CHOICES})
+    cities=Companion.CITY_CHOICES
+    genders=Companion.GENDER_CHOICES
+
+    return render(request,"main_app/home.html",{'cities': cities , 'genders': genders})
 
 def companions_list_view (request:HttpRequest):
-# adding filter code here 
-    companions = Companion.objects.all()  # Start with all companions
-    # skills = Skill.objects.all()  # Get all skills for the dropdown
 
-    # # Get filter criteria from query parameters
-    # city = request.GET.get('city')
-    # skill_name = request.GET.get('skills')
-    # disability_type = request.GET.get('disability_type')
-
-    # # Apply filters based on user selection
-    # if city:
-    #     companions = companions.filter(city=city)
-    # if skill_name:
-    #     companions = companions.filter(skills__name=skill_name)
-    # if disability_type:
-    #     companions = companions.filter(companion__disabilityuser__disability_type=disability_type)
-
-    # return render(request, "main_app/companions_list.html", {
-    #     'companions': companions,
-    #     'skills': skills,
-    #     'selected_city': city,
-    #     'selected_skill': skill_name,
-    #     'selected_disability': disability_type,
-    #     'CITY_CHOICES': Companion.CITY_CHOICES,  # Pass CITY_CHOICES to the template
-    # })
+    companions = Companion.objects.all()  
+  
 
     search_city = request.GET.get('city', '')
     search_gender = request.GET.get('gender', '')
@@ -54,8 +32,8 @@ def companions_list_view (request:HttpRequest):
         companions = companions.filter(disability_type=search_disability_type)
 
     # Get all unique cities for the filter dropdown
-    cities = Companion.objects.values_list('city', flat=True).distinct()
-
+    cities = Companion.CITY_CHOICES
+    print(Companion.CITY_CHOICES)
     return render(request, 'main_app/companions_list.html', {
         'companions': companions,
         'cities': cities,
